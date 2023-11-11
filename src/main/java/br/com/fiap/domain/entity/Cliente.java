2f68b722c91e969fa5b5370370aaf7259c2d1be9
murilomunari <129402165+murilomunari@users.noreply.github.com>
2023-11-11 15:26:19 -0300

    acabei de fato

--- src/main/java/br/com/fiap/domain/entity/Cliente.java
+++ src/main/java/br/com/fiap/domain/entity/Cliente.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@RequiredArgsConstructor
+
 public class Cliente {
 
     private Long id;
@@ -17,6 +15,61 @@ public class Cliente {
 
     private String cpf;
 
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String endereco, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Cliente setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Cliente setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Cliente setEndereco(String endereco) {
+        this.endereco = endereco;
+        return this;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Cliente setCpf(String cpf) {
+        this.cpf = cpf;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
 }
 
 
